a75cec9baa9340722fe409c33e2619ead6f3a3ba
Tudor <if20376@bristol.ac.uk>
2024-05-24 16:39:12 +0100

    Updated UI and dummy functionality added for it

--- Reactivities/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Reactivities/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f036a01e0389666cf81b6399597884150ee58b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16271b94656eb3ea32f081d80fd5f606b61b801b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
