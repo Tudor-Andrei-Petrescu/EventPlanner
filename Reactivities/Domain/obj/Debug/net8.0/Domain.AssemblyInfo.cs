eeb972a85d9af22b13180c9bbbe726474afa4f57
Tudor <if20376@bristol.ac.uk>
2024-05-24 21:29:16 +0100

    Added CRUD functionality to the UI

--- Reactivities/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Reactivities/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f036a01e0389666cf81b6399597884150ee58b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fd1a672929cc7f6f669dbd513b8581b7a8137c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
 import { Activity } from "../../../app/models/activity";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 
@@ -7,9 +7,19 @@ interface Props{
   activities: Activity[];
   selectActivity : (id: string) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
-export default function ActivityList({activities, selectActivity, deleteActivity}: Props){
+export default function ActivityList({activities, selectActivity, deleteActivity, submitting}: Props){
+
+    const [target, setTarget] = useState('');
+
+    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
+
+      setTarget(e.currentTarget.name);
+      deleteActivity(id);
+    }
+
 
     return (
       <Segment>
@@ -24,8 +34,12 @@ export default function ActivityList({activities, selectActivity, deleteActivity
                       <div>{activity.city}, {activity.venue}</div>
                     </Item.Description>
                     <Item.Extra>
-                      <Button  onClick ={() => deleteActivity(activity.id)} floated='right' content='Delete' color='red'/>
-                      <Button  onClick ={() => selectActivity(activity.id)} floated='right' content='View' color='blue'/>
+                      <Button 
+                      loading = {submitting && target === activity.id} 
+                      onClick ={(e) => handleActivityDelete(e, activity.id)} 
+                      name = {activity.id}
+                      floated='right' content='Delete' color='red'/>
+                      <Button onClick ={() => selectActivity(activity.id)} floated='right' content='View' color='blue'/>
                       <Label basic content={activity.category}/>
                     </Item.Extra>
                   </Item.Content>
