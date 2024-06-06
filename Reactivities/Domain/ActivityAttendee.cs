498a9a58c6fc8028072481d1bc2e53c21f471182
Tudor <if20376@bristol.ac.uk>
2024-06-06 18:09:50 +0100

    Added EF relationships, update attendance doesn't work

---
+++ Reactivities/Domain/ActivityAttendee.cs
@@ -0,0 +1,13 @@
+namespace Domain
+{
+    public class ActivityAttendee
+    {
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public int MyProperty { get; set; }
+        public Guid ActivityId { get; set; }
+        public Activity Activity { get; set; }
+        public bool isHost { get; set; }
+        
+    }
+}
