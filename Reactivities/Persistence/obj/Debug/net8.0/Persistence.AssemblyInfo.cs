592f23e40cd264b9ad0b0b4978c560050da1e003
Tudor <if20376@bristol.ac.uk>
2024-06-06 21:40:04 +0100

    EF relationships working

--- Reactivities/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Reactivities/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3de7f40b07bb7c244a2a8f1b917edb6e2d50aa3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe701ad265188a6720d97e27e337848688d5d834")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
