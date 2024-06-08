be07f43f82e8f94062adae9a2b71180900c453d7
Tudor <if20376@bristol.ac.uk>
2024-06-08 01:03:52 +0100

    Added backend capability for profile images

--- Reactivities/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Reactivities/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+592f23e40cd264b9ad0b0b4978c560050da1e003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b28a4cfeac295ecafbf87cea762f67e25eea88")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
