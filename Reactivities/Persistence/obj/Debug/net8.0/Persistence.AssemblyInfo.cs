dc98ca5b230663ef7068e1948b83a3227d128750
Tudor <if20376@bristol.ac.uk>
2024-06-10 22:11:17 +0100

    About section implementation

--- Reactivities/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Reactivities/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be07f43f82e8f94062adae9a2b71180900c453d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+739b0fc6da63827434b08f9c74498bf8d21cb273")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
